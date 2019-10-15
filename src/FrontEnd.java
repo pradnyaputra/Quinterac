360ab160a7c5712c1a5a46bf69a7bb57842f1c4f
Tyler Elliott <tyler.elliott@queensu.ca>
2019-10-14 21:29:43 -0400

    Add queue implemented as linkedlist

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,5 +1,9 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class FrontEnd {
+
+	public static Queue transactionQueue = new LinkedList();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
