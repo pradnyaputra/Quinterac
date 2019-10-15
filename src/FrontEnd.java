f04d20b0116aa8639a4f3f4b4c2919f9fa0a6c73
Tyler Elliott <tyler.elliott@queensu.ca>
2019-10-14 21:43:00 -0400

    Add the default screen awaiting user input

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class FrontEnd {
 
@@ -7,36 +8,75 @@ public class FrontEnd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		login();
 		System.out.println("hi");
 	}
 
-	public void login() {
-
+	public static void login() {
+		defaultScreen();
 	}
 
-	public void logout() {
+	public static void logout() {
+		System.out.println("logout");
+		defaultScreen();
 	}
 
-	public void createacct() {
-
+	public static void createacct() {
+		System.out.println("createacct");
+		defaultScreen();
 	}
 
-	public void deleteacct() {
-
+	public static void deleteacct() {
+		System.out.println("deleteacct");
+		defaultScreen();
 	}
 
-	public void deposit() {
-
+	public static void deposit() {
+		System.out.println("deposit");
+		defaultScreen();
 	}
 
-	public void withdraw() {
-
+	public static void withdraw() {
+		System.out.println("withdraw");
+		defaultScreen();
 	}
 
-	public void transfer() {
-
+	public static void transfer() {
+		System.out.println("transfer");
+		defaultScreen();
 	}
 
+
+	/*
+	* Common helper functions
+	* */
+	public static void defaultScreen() {
+		Scanner input = new Scanner(System.in);
+		switch(input.nextLine()) {
+			case "logout":
+				logout();
+				break;
+			case "createacct":
+				createacct();
+				break;
+			case "deleteacct":
+				deleteacct();
+				break;
+			case "deposit":
+				deposit();
+				break;
+			case "withdraw":
+				withdraw();
+				break;
+			case "transfer":
+				transfer();
+				break;
+			default:
+				System.out.println("Please enter one of the commands as input");
+				defaultScreen();
+		}
+
+	}
 
 
 }
