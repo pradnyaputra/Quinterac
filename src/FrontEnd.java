2881e98f8a0193fd9a3c40a61d1fb27722fa5c1d
Pradnya Sadhu Putra <pradnya.putra@gmail.com>
2019-10-18 04:01:49 -0400

    updating documentation and commenting

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,3 +1,7 @@
+/*
+ * Developed by YES-MEN team
+ */
+
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -5,6 +9,7 @@ import java.util.Scanner;
 
 public class FrontEnd {
 
+	//declaring global values that will be used through direct reference throughout the program
 	private static Queue<String> tsfQueue = new LinkedList<>();
 	private static boolean loggedOut = true;
 	private static String accountListFileLocation = "";
@@ -22,6 +27,7 @@ public class FrontEnd {
 		startUp();
 	}
 
+	//startUp method introduces the program and requests for initial user commands
 	private static void startUp() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to Quinterac, developed by YES-MEN");
@@ -29,8 +35,14 @@ public class FrontEnd {
 		while (!input.nextLine().equals("login")) {
 			System.out.println("The only valid command is login");
 		}
+		
+		//agent boolean value to determine the mode (agent or !agent (aka machine))
 		boolean agent = login();
+		
+		//While loop will loop continuously (until logout is called), enabling the execution of several methods within a single session.
 		while (!loggedOut) {
+			
+			//switch case to check user input and execute the corresponding method
 			switch (input.nextLine()) {
 				case "logout":
 					logout();
@@ -56,51 +68,67 @@ public class FrontEnd {
 		}
 	}
 
+	//login method called by startUp method, asking for user mode
 	private static boolean login() {
 		System.out.println("Do you want a machine or agent session?");
 		Scanner input = new Scanner(System.in);
 		boolean agent;
 		String sessionType = input.nextLine();
+		
+		//While loop to ensure either machine or agent is entered (all other options invalid)
 		while (!sessionType.equals("machine") && !sessionType.equals("agent")) {
 			System.out.println("The only valid options are 'machine' or 'agent'");
 			sessionType = input.nextLine();
 		}
+		//determines whether agent boolean is true or false based on input
 		agent = sessionType.equals("agent");
+		
+		//Sets the variable loggedOut to false, ensuring that multiple methods can be executed in a single session
 		loggedOut = false;
 		System.out.println("Logged in");
 		return agent;
 	}
 
+	//logout method notifies user of end of session, adds EOS to tsfQueuem, and writes to transaction summary file
 	private static void logout() {
 		System.out.println("The session has been closed for the day");
 		String endOfSession = "EOS";
 		tsfQueue.add(endOfSession);
+		
+		//trycatch to write data from tsfQueue into transaction summary file
 		try {
 			writeTransactionsToSummaryFile("transactionSummaryFile.txt");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		//sets the logout variable to true, preventing the main menu from looping
 		loggedOut = true;
 		startUp();
 	}
 
+	//createAcct creates a new account using user input and adds transaction code to tsfQueue
 	private static void createAcct(boolean agent) {
 		Scanner input = new Scanner(System.in);
 
 		String accName;
 		String accNum;
 		String tsfData;
+		
+		//checking whether in agent or machine mode
 		if (!agent) {
 			System.out.println("Please use agent mode to access this command.");
 			return;
-		} else {
+		} else {// machine mode
 			System.out.println("Please enter the new account number: ");
 			accNum = input.nextLine();
 
+			//proceeds with account creation after account number has been validated, and there is no current account with that account number
 			if (accountNumberValid(accNum) && !accountNumberExists(accNum)) {
 				System.out.println("Please enter an account name: ");
 				accName = input.nextLine();
 
+				//verifies the validity of the account name, and then adds the corresponding transaction code tsfQueue
 				if (accountNameValid(accName)) {
 					tsfData = "NEW " + accNum + " 000 0000000 " + accName;
 					tsfQueue.add(tsfData);
@@ -118,12 +146,14 @@ public class FrontEnd {
 
 	}
 
+	//deleteAcct deletes an existing account and adds transaction code to tsfQueue
 	private static void deleteacct(boolean agent) {
 		Scanner input = new Scanner(System.in);
 		String accName;
 		String accNum;
 		String tsfData;
 
+		//checking whether in agent or machine mode
 		if (!agent) {
 			System.out.println("Please use agent mode to access this command.");
 			return;
@@ -131,10 +161,12 @@ public class FrontEnd {
 			System.out.println("Please enter the your account number: ");
 			accNum = input.nextLine();
 
+			//proceeds with account deletion after account number has been validated, and there is currently an account with that account number
 			if (accountNumberValid(accNum) && accountNumberExists(accNum)) {
 				System.out.println("Please enter your account name: ");
 				accName = input.nextLine();
-
+				
+				//verifies the validity of the account name, and then adds the corresponding transaction code tsfQueue
 				if (accountNameValid(accName)) {
 					tsfData = "DEL " + accNum + " 000 0000000 " + accName;
 					tsfQueue.add(tsfData);
@@ -151,6 +183,7 @@ public class FrontEnd {
 		}
 	}
 
+	//deposits money into an account and adds transaction code to tsfQueue
 	private static void deposit(boolean agent) {
 		Scanner input = new Scanner(System.in);
 		String amount;
@@ -160,12 +193,17 @@ public class FrontEnd {
 		System.out.println("Please enter the your account number: ");
 		accNum = input.nextLine();
 
+		//proceeds with deposit after account number has been validated, and there is currently an account with that account number
 		if (accountNumberValid(accNum) && accountNumberExists(accNum)) {
 			System.out.println("Please enter the amount you wish to deposit: ");
 			amount = input.nextLine();
 
 			if (isAllDigits(amount)) {
+				
+				//checking whether in agent or machine mode
 				if (agent) {
+					
+					//verifies the validity of the amount to deposit, and then adds the corresponding transaction code tsfQueue
 					if (Integer.parseInt(amount) >= 0 && Integer.parseInt(amount) <= 99999999) {
 						tsfData = "DEP " + accNum + " " + amount + " 0000000 ***";
 						tsfQueue.add(tsfData);
@@ -176,8 +214,12 @@ public class FrontEnd {
 						return;
 					}
 				} else {// machine mode
+					
+					//verifies the validity of the amount to deposit, and then adds the corresponding transaction code tsfQueue
 					if (Integer.parseInt(amount) >= 0 && Integer.parseInt(amount) <= 200000) {
 						int dailyTotal = 0;
+						
+						//loops through tsfQueue to sum the amount of cash deposited into that account so far in the day
 						for (int i = 0; i < tsfQueue.size(); i++) {
 							String queueVal = tsfQueue.poll();
 							String[] queueValArr = queueVal.split(" ");
@@ -186,6 +228,8 @@ public class FrontEnd {
 							}
 							tsfQueue.add(queueVal);
 						}
+						
+						//checks whether the account has exceeded this specific daily transaction limit
 						if ((dailyTotal + Integer.parseInt(amount)) > 500000) {
 							System.out.println("ERROR: Can’t deposit more than $5000 in one day in ATM mode");
 							return;
@@ -211,6 +255,7 @@ public class FrontEnd {
 		}
 	}
 
+	//withdraws from an account and adds transaction code to tsfQueue
 	private static void withdraw(boolean agent) {
 		Scanner input = new Scanner(System.in);
 		String amount;
@@ -220,12 +265,17 @@ public class FrontEnd {
 		System.out.println("Please enter the your account number: ");
 		accNum = input.nextLine();
 
+		//proceeds with withdrawal after account number has been validated, and there is currently an account with that account number
 		if (accountNumberValid(accNum) && accountNumberExists(accNum)) {
 			System.out.println("Please enter the amount you wish to withdraw in cents: ");
 			amount = input.nextLine();
 
 			if (isAllDigits(amount)) {
+				
+				//checking whether in agent or machine mode
 				if (agent) {
+					
+					//verifies the validity of the amount to withdraw, and then adds the corresponding transaction code tsfQueue
 					if (Integer.parseInt(amount) >= 0 && Integer.parseInt(amount) <= 99999999) {
 						tsfData = "WDR 0000000 " + amount + " " + accNum + " ***";
 						tsfQueue.add(tsfData);
@@ -236,8 +286,12 @@ public class FrontEnd {
 						return;
 					}
 				} else {// machine mode
+					
+					//verifies the validity of the amount to withdraw, and then adds the corresponding transaction code tsfQueue
 					if (Integer.parseInt(amount) >= 0 && Integer.parseInt(amount) <= 100000) {
 						int dailyTotal = 0;
+						
+						//loops through tsfQueue to sum the amount of cash withdrawn into that account so far in the day
 						for (int i = 0; i < tsfQueue.size(); i++) {
 							String queueVal = tsfQueue.poll();
 							String[] queueValArr = queueVal.split(" ");
@@ -246,6 +300,8 @@ public class FrontEnd {
 							}
 							tsfQueue.add(queueVal);
 						}
+						
+						//checks whether the account has exceeded this specific daily transaction limit
 						if ((dailyTotal + Integer.parseInt(amount)) > 500000) {
 							System.out.println("ERROR: Can’t withdraw more than $5000 in one day in ATM mode");
 							return;
@@ -284,13 +340,17 @@ public class FrontEnd {
 		System.out.println("Please enter the account you want to transfer to: ");
 		accNumTo = input.nextLine();
 
-
+		//proceeds with transfer after account numbers have been validated, and there are currently accounts with those account numbers
 		if (accountNumberValid(accNumTo) && accountNumberExists(accNumTo) && accountNumberValid(accNumFrom) && accountNumberExists(accNumFrom)) {
 			System.out.println("Please enter the amount you wish to transfer in cents: ");
 			amount = input.nextLine();
 
 			if (isAllDigits(amount)) {
+				
+				//checking whether in agent or machine mode
 				if (agent) {
+					
+					//verifies the validity of the amount to transferred, and then adds the corresponding transaction code tsfQueue
 					if (Integer.parseInt(amount) >= 0 && Integer.parseInt(amount) <= 99999999) {
 						tsfData = "XFR " + accNumTo + " " + amount + " " + accNumFrom + " ***";
 						tsfQueue.add(tsfData);
@@ -301,8 +361,12 @@ public class FrontEnd {
 						return;
 					}
 				} else {// machine mode
+					
+					//verifies the validity of the amount to transfer, and then adds the corresponding transaction code tsfQueue
 					if (Integer.parseInt(amount) >= 0 && Integer.parseInt(amount) <= 1000000) {
 						int dailyTotal = 0;
+						
+						//loops through tsfQueue to sum the amount of cash transferred from that account so far in the day
 						for (int i = 0; i < tsfQueue.size(); i++) {
 							String queueVal = tsfQueue.poll();
 							String[] queueValArr = queueVal.split(" ");
@@ -311,6 +375,8 @@ public class FrontEnd {
 							}
 							tsfQueue.add(queueVal);
 						}
+						
+						//checks whether the account has exceeded this specific daily transaction limit
 						if ((dailyTotal + Integer.parseInt(amount)) > 1000000) {
 							System.out.println("ERROR: Can’t transfer more than $10000 in one day in ATM mode");
 							return;
@@ -340,7 +406,10 @@ public class FrontEnd {
 	 * Common helper functions
 	 */
 
+	//checks whether the string entered consists only of integers or not
 	private static boolean isAllDigits(String number) {
+		
+		//loops through all characters of the string and checks if it is a digit
 		for (Character x : number.toCharArray()) {
 			if (!Character.isDigit(x)) {
 				return false;
@@ -349,6 +418,7 @@ public class FrontEnd {
 		return true;
 	}
 
+	//checks whether the account number is valid by following the constraints
 	private static boolean accountNumberValid(String number) {
 		// new account number is exactly seven decimal digits not beginning with 0
 		// (e.g., 1234567)
@@ -365,28 +435,34 @@ public class FrontEnd {
 
 	}
 
+	//checks whether an account number exists in the valid account list file
 	private static boolean accountNumberExists(String number) {
 		Scanner file = null;
+		//trycatch to open the valid account list file
 		try {
 			file = new Scanner(new FileInputStream("validAccountList.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("ERROR: " + e.getMessage());
 		}
 
-
+		//while loop to ensure all lines are read within the file
 		while (file.hasNextLine()) {
 			String line = file.nextLine();
+			//will return true if the account number is found
 			if (line.equals(number)) {
 				return true;
 			}
 		}
 		file.close();
+		//will return false if the while loop exits without finding the account number
 		return false;
 
 	}
 
+	//Checks whether an account name is valid by following constraints
 	private static boolean accountNameValid(String name) {
 
+		//returns a boolean variable if the following conditions are true
 		return (name.matches("[a-zA-Z0-9]+"))
 				&& (name.length() <= 30)
 				&& (name.length() >= 3)
@@ -395,8 +471,11 @@ public class FrontEnd {
 
 	}
 
+	//Accepts filename and then empties the tsfQueue while writing tsfQueue's data into the transaction summary file
 	private static void writeTransactionsToSummaryFile(String fileName) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+		
+		//while loop to ensure the queue is processed until it is fully emptied
 		while (!tsfQueue.isEmpty()) {
 			writer.write(tsfQueue.poll());
 			writer.newLine();
