773df97b825d8dabb676739e37031e55df6ee3c2
Tony Chen <anacrusy.tc@gmail.com>
2019-11-06 21:40:16 -0500

    updated backend to process merged transactions

--- src/main/java/BackEnd.java
+++ src/main/java/BackEnd.java
@@ -20,6 +20,7 @@ public class BackEnd {
             mergedTransactionSummaryFile = args[1];
         } 
 		accounts = readOldMasterAccountsFile(oldMasterAccountsFile);
+		processMergedTransactions(mergedTransactionSummaryFile);
 		
 		for (Integer i : accounts.keySet()) {
 			  System.out.println("key: " + i + " value: " + accounts.get(i).getBalance() + " " + accounts.get(i).getAccountName() );
@@ -30,7 +31,6 @@ public class BackEnd {
 	public static HashMap<Integer, Account> readOldMasterAccountsFile(String filename) {
 		HashMap<Integer, Account> temp = new HashMap<Integer, Account>();
 		Scanner file = null;
-        //trycatch to open the valid account list file
         try {
             file = new Scanner(new FileInputStream(filename));
             //while loop to ensure all lines are read within the file
@@ -38,7 +38,6 @@ public class BackEnd {
             while (file.hasNextLine()) {
                 String line = file.nextLine();
                 String words[] = line.split(" ");
-                //adds every account number from the valid account list file to the hashset
                 Account tempAccount = new Account(Integer.parseInt(words[0]), Integer.parseInt(words[1]), words[2]);
                 temp.put(Integer.parseInt(words[0]), tempAccount);
             }
@@ -48,4 +47,97 @@ public class BackEnd {
         }
         return temp;
 	}
+	
+	public static void processMergedTransactions(String filename) {
+		Scanner file = null;
+        try {
+            file = new Scanner(new FileInputStream(filename));
+            //while loop to ensure all lines are read within the file
+
+            while (file.hasNextLine()) {
+                String line = file.nextLine();
+                String words[] = line.split(" ");
+                //adds every account number from the valid account list file to the hashset
+                switch (words[0]) {
+	                case "NEW":
+	                    createAcct(Integer.parseInt(words[1]), words[4]);
+	                    break;
+	                case "DEP":
+	                	deposit(Integer.parseInt(words[1]), Integer.parseInt(words[2]), words[4]);
+	                	break;
+	                case "WDR":
+	                	withdraw(Integer.parseInt(words[1]), Integer.parseInt(words[2]), words[4]);
+	                    break;
+	                case "XFR":
+	                	transfer(Integer.parseInt(words[1]), Integer.parseInt(words[2]), Integer.parseInt(words[3]));
+	                	break;
+	                case "DEL":
+	                    deleteAcct(Integer.parseInt(words[1]), words[4]);
+	                    break;
+	                case "EOS":
+	                	break;
+	                default:
+	                    System.out.println(line);
+                }
+            }
+            file.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+		
+	}
+
+	private static void deleteAcct(int accountNumber, String accountName) {
+		Account tempAccount = accounts.get(accountNumber); 
+		if (!tempAccount.getAccountName().equals(accountName)) {
+			System.out.println("Error, name does not match");
+			System.out.println("should be " + accountName + tempAccount.getAccountName());
+			return;
+		}
+		accounts.remove(accountNumber);
+		
+	}
+
+	private static void transfer(int accountNumberFrom, int amount, int accountNumberTo) {
+		Account fromAccount = accounts.get(accountNumberFrom); 
+		Account toAccount = accounts.get(accountNumberTo); 
+		
+		if (fromAccount.getBalance() - amount < 0) {
+			System.out.println("Negative balance");
+		}
+		else {
+			fromAccount.setBalance(fromAccount.getBalance() - amount);
+			toAccount.setBalance(toAccount.getBalance() + amount);
+		}
+		
+	}
+
+	private static void withdraw(int accountNumber, int amount, String accountName) {
+		Account tempAccount = accounts.get(accountNumber); 
+		if (!tempAccount.getAccountName().equals(accountName)) {
+			System.out.println("Error, name does not match");
+			return;
+		}
+		if (tempAccount.getBalance() - amount < 0) {
+			System.out.println("Negative balance");
+		}
+		else {
+			tempAccount.setBalance(tempAccount.getBalance() - amount);
+		}
+	}
+
+	private static void deposit(int accountNumber, int amount, String accountName) {
+		Account tempAccount = accounts.get(accountNumber); 
+		if (!tempAccount.getAccountName().equals(accountName)) {
+			System.out.println("Error, name does not match");
+			return;
+		}
+		tempAccount.setBalance(amount + tempAccount.getBalance());
+	}
+
+	private static void createAcct(int accountNumber, String accountName) {
+		Account tempAccount = new Account(accountNumber, 0, accountName);
+		accounts.put(accountNumber, tempAccount);
+	}
+	
 }
