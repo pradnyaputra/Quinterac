0d90e7cfcd09113ce77aca9b0a81008085ce3079
Tyler Elliott <tyler.elliott@queensu.ca>
2019-12-03 21:31:11 -0500

    Add weekly and daily script interaction

--- src/main/java/dailyScript.py
+++ src/main/java/dailyScript.py
@@ -9,40 +9,59 @@ def mergedTsfFiles():
     contents = tsfFile.read()
     mergedTsf.write(contents)
 
+def first_day():
+    print("--- Day 1 ---")
 
-# os.system("./main/java/FrontEnd")
+    print("Ran session 1 automatically")
+    subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt",
+                   input="login\nagent\ncreateacct\n9999999\nfoo\nlogout", text=True)
+    mergedTsfFiles()  # call this after every logout
 
-# subprocess.check_call("dir")
+    print("Ran session 2 automatically")
+    subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt",
+                   input="login\nagent\ncreateacct\n7777777\nbar\nlogout", text=True)
+    mergedTsfFiles()  # call this after every logout
 
-# print(os.path.realpath("../../../" + __file__))
+    print("Ran session 3 manually")
+    subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
 
+    mergedTsfFiles()  # call this after every logout
 
-
-subprocess.run("javac FrontEnd.java")
-
-# subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
-subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt", input="login\nagent\ncreateacct\n9999999\ncoo\nlogout", text=True)
-
-# sys.stdin.write("login")
-
-# sys.stdin = "login"
-
+    print("Ran backend for day")
+    subprocess.run("javac BackEnd.java")
+    subprocess.run("java BackEnd ../../../oldMasterAccounts.txt ../../../mergedTransactionSummaryFile.txt")
 
 
+def other_days():
+    print("--- Day " + sys.argv[1] + " ---")
 
-print("after run before merge")
+    print("Ran session 1 automatically")
+    subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt",
+                   input="login\nagent\ndeposit\n9999999\n100000\nfoo\nlogout", text=True)
+    mergedTsfFiles()  # call this after every logout
 
-mergedTsfFiles() #call this after every logout
+    print("Ran session 2 automatically")
+    subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt",
+                   input="login\nagent\ndeposit\n7777777\n87654321\nbar\nlogout", text=True)
 
-print("after merge")
+    mergedTsfFiles()  # call this after every logout
 
-subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
+    print("Ran session 3 manually")
+    subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
 
-mergedTsfFiles() #call this after every logout
+    mergedTsfFiles()  # call this after every logout
 
-subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
+    print("Ran backend for day")
+    subprocess.run("javac BackEnd.java")
+    subprocess.run("java BackEnd ../../../oldMasterAccounts.txt ../../../mergedTransactionSummaryFile.txt")
 
-mergedTsfFiles() #call this after every logout
 
-subprocess.run("javac BackEnd.java")
-subprocess.run("java BackEnd ../../../oldMasterAccounts.txt ../../../mergedTransactionSummaryFile.txt")
+# Monday (first day)
+if len(sys.argv) >= 2:
+    if sys.argv[1] == "1":
+        # it is the first day
+        first_day();
+    else:
+        other_days();
+else:
+    print("Script malfunction, specify day")
