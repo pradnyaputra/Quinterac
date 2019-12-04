50444f8d31c52def0e15f4391c6f14e4f0a23ced
Tyler Elliott <tyler.elliott@queensu.ca>
2019-12-03 20:54:50 -0500

    Finish daily script

--- src/main/java/dailyScript.py
+++ src/main/java/dailyScript.py
@@ -1,7 +1,14 @@
 import os
 import subprocess
+import sys
 
 mergedTsf = open("mergedTransactionSummaryFile.txt", "w+")
+
+def mergedTsfFiles():
+    tsfFile = open("../../../transactionSummaryFile.txt", "r")
+    contents = tsfFile.read()
+    mergedTsf.write(contents)
+
 
 # os.system("./main/java/FrontEnd")
 
@@ -9,11 +16,33 @@ mergedTsf = open("mergedTransactionSummaryFile.txt", "w+")
 
 # print(os.path.realpath("../../../" + __file__))
 
-#subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
 
-def mergedTsfFiles():
-    tsfFile = open("../../../transactionSummaryFile.txt", "r")
-    contents = tsfFile.read()
-    mergedTsf.write(contents)
+
+subprocess.run("javac FrontEnd.java")
+
+# subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
+subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt", input="login\nagent\ncreateacct\n9999999\ncoo\nlogout", text=True)
+
+# sys.stdin.write("login")
+
+# sys.stdin = "login"
+
+
+
+
+print("after run before merge")
 
 mergedTsfFiles() #call this after every logout
+
+print("after merge")
+
+subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
+
+mergedTsfFiles() #call this after every logout
+
+subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
+
+mergedTsfFiles() #call this after every logout
+
+subprocess.run("javac BackEnd.java")
+subprocess.run("java BackEnd ../../../oldMasterAccounts.txt ../../../mergedTransactionSummaryFile.txt")
