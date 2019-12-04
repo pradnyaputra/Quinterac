9d47d38afc6543a0b1341e3e6db2eb21da3c6d8d
Tyler Elliott <tyler.elliott@queensu.ca>
2019-12-03 22:16:32 -0500

    Update dailyScript to append to file

--- src/main/java/dailyScript.py
+++ src/main/java/dailyScript.py
@@ -2,25 +2,27 @@ import os
 import subprocess
 import sys
 
-mergedTsf = open("mergedTransactionSummaryFile.txt", "w+")
+mergedTsf = open("../../../mergedTransactionSummaryFile.txt", "a+")
 
 def mergedTsfFiles():
     tsfFile = open("../../../transactionSummaryFile.txt", "r")
     contents = tsfFile.read()
+    print(contents)
     mergedTsf.write(contents)
 
 def first_day():
     print("--- Day 1 ---")
+    subprocess.run("javac FrontEnd.java")
 
     print("Ran session 1 automatically")
-    subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt",
-                   input="login\nagent\ncreateacct\n9999999\nfoo\nlogout", text=True)
-    mergedTsfFiles()  # call this after every logout
+    # subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt",
+    #                input="login\nagent\ncreateacct\n9999999\nfoo\nlogout", text=True)
+    # mergedTsfFiles()  # call this after every logout
 
     print("Ran session 2 automatically")
-    subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt",
-                   input="login\nagent\ncreateacct\n7777777\nbar\nlogout", text=True)
-    mergedTsfFiles()  # call this after every logout
+    # subprocess.run("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt",
+    #                input="login\nagent\ncreateacct\n7777777\nbar\nlogout", text=True)
+    # mergedTsfFiles()  # call this after every logout
 
     print("Ran session 3 manually")
     subprocess.check_call("java FrontEnd ../../../validAccountList.txt ../../../transactionSummaryFile.txt")
@@ -61,7 +63,7 @@ if len(sys.argv) >= 2:
     if sys.argv[1] == "1":
         # it is the first day
         first_day();
-    else:
-        other_days();
+    # else:
+        # other_days();
 else:
     print("Script malfunction, specify day")
