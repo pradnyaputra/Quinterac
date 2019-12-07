3db6a05b617ba72f4c246a06d564856dcc8cd7c2
Armaan Merchant <amerchant98@gmail.com>
2019-12-06 20:50:57 -0500

    removed timer functions in script

--- weeklyScript.py
+++ weeklyScript.py
@@ -1,19 +1,13 @@
 import subprocess
-import time
 
 # Monday
 subprocess.run("python dailyScript.py 1")
-
-time.sleep(30)
 # Tuesday
 subprocess.run("python dailyScript.py 2")
-time.sleep(30)
 # Wednesday
 subprocess.run("python dailyScript.py 3")
-time.sleep(30)
 # Thursday
 subprocess.run("python dailyScript.py 4")
-time.sleep(30)
 # Friday
 subprocess.run("python dailyScript.py 5")
 
